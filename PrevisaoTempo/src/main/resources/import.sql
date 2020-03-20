5643af26b539b1eeca622448daf171df01a3f937
Andre de Camargo <camargo_and@hotmail.com>
2020-03-20 20:25:55 -0300

    Versão Final

---
+++ PrevisaoTempo/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into prev_tempo(id, dia_semana, temp_min, temp_max, umidade, descricao) values(1, '21/03 Sabado' , 19.5 , 22.1 , 100 , 'Chuva Leve');
+insert into prev_tempo(id, dia_semana, temp_min, temp_max, umidade, descricao) values(2, '22/03 Domingo' , 18.8 , 24.7 , 75 , 'Nublado');
+insert into prev_tempo(id, dia_semana, temp_min, temp_max, umidade, descricao) values(3, '23/03 Segunda' , 18.9 , 23.7 , 77 , 'Chuva Leve');
+insert into prev_tempo(id, dia_semana, temp_min, temp_max, umidade, descricao) values(4, '24/03 Terça' , 17.3 , 24.4 , 22 , 'Poucas Nuvens');
+insert into prev_tempo(id, dia_semana, temp_min, temp_max, umidade, descricao) values(5, '25/03 Quarta' , 17.4 , 24.4 , 0 , 'Céu Limpo');
+insert into prev_tempo(id, dia_semana, temp_min, temp_max, umidade, descricao) values(6, '26/03 Quinta' , 18.1 , 24.4 , 0 , 'Céu Limpo');
+insert into prev_tempo(id, dia_semana, temp_min, temp_max, umidade, descricao) values(7, '27/03 Sexta' , 18.9 , 25.5 , 100 , 'Chuva Leve');
---
+++ PrevisaoTempo/src/test/java/br/usjt/PrevisaoTempo/PrevisaoTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usjt.PrevisaoTempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisaoTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
